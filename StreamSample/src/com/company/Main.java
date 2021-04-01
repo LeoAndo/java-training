96bc13c7981fc812e49095beff3556aed2df3ad2
Reo Ando <ando.reo420@gmail.com>
2021-04-01 20:50:14 +0900

    Listでstream APIを使うパターン

--- StreamSample/src/com/company/Main.java
+++ StreamSample/src/com/company/Main.java
@@ -1,10 +1,21 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
-    private static String[] dataset = {"a", "ab", "abc", "abcd", "abcde", "abcdef"};
+    static List<String> dataset = Arrays.asList(
+            "1",
+            "12",
+            "123",
+            "1234",
+            "12345",
+            "123456",
+            "1234567",
+            "12345678",
+            "123456789",
+            "1234567890");
 
     public static void main(String[] args) {
         System.out.println(filterByLength());
@@ -12,6 +23,10 @@ public class Main {
 
     private static long filterByLength() {
         // 元データのdatasetは変更せずに処理結果を返す.
-        return Arrays.stream(dataset).filter(data -> 3 < data.length()).count();
+        long start = System.currentTimeMillis();
+        long count = dataset.stream().filter(data -> 3 < data.length()).count();
+        long end = System.currentTimeMillis();
+        System.out.println("filterByLength 処理時間: " + (end - start) + " milliseconds.");
+        return count;
     }
 }
