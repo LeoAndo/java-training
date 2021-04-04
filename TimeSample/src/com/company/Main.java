6e9eeb91404977adcf0c6a17a0046590c9bcf647
Reo Ando <ando.reo420@gmail.com>
2021-04-04 15:03:25 +0900

    ローカル日付を扱う

--- TimeSample/src/com/company/Main.java
+++ TimeSample/src/com/company/Main.java
@@ -2,6 +2,10 @@ package com.company;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,10 +25,11 @@ public class Main {
         });
         System.out.println(millis);
         System.out.println(millis2);
-
         if (millis * 2 <= millis2) {
             System.out.println("millis2の方が処理時間が２倍以上かかっている.");
         }
+
+        exampleLocalDate(() -> DateTimeFormatter.ISO_LOCAL_DATE);
     }
 
     /**
@@ -35,5 +40,14 @@ public class Main {
         runnable.run();
         Instant end = Instant.now();
         return Duration.between(start, end).toMillis();
+    }
+
+    private static void exampleLocalDate(Supplier<DateTimeFormatter> formatter) {
+        LocalDate today = LocalDate.now();
+        LocalDate tomorrow = today.plusDays(1);
+        LocalDate odaNobunagasBirthday = LocalDate.of(1534, Month.JUNE, 23);
+        System.out.println("today: " + formatter.get().format(today));
+        System.out.println("tomorrow: " + formatter.get().format(tomorrow));
+        System.out.println("odaNobunagasBirthday: " + formatter.get().format(odaNobunagasBirthday));
     }
 }
