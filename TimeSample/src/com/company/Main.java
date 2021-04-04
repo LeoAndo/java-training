28668f15fe99465f1a33f4b7e41a6ee672088365
Reo Ando <ando.reo420@gmail.com>
2021-04-04 11:47:34 +0900

    java.time APIで処理時間の計測を行う

--- TimeSample/src/com/company/Main.java
+++ TimeSample/src/com/company/Main.java
@@ -1,7 +1,27 @@
 package com.company;
 
-public class Main {
+import java.time.Duration;
+import java.time.Instant;
 
+public class Main {
     public static void main(String[] args) {
+        long millis = measureTimeMillis(() -> {
+            try {
+                Thread.sleep(2_000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        System.out.println(millis);
+    }
+
+    /**
+     * 処理時間を計測する.
+     */
+    private static long measureTimeMillis(Runnable runnable) {
+        Instant start = Instant.now();
+        runnable.run();
+        Instant end = Instant.now();
+        return Duration.between(start, end).toMillis();
     }
 }
