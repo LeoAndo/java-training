2f8c712b99d59686b7253e13ad6cdf25b6c03fff
LeoAndo <ando@LeoAndonoMacBook-Pro.local>
2021-04-07 20:23:18 +0900

    remove unused import.

--- Java8PlayGround/src/App.java
+++ Java8PlayGround/src/App.java
@@ -1,9 +1,7 @@
-import java.security.acl.Permission;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.IntStream;
 
 public class App {
