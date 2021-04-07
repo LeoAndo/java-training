07ebfe4c0866da7fbfe3396c4a24c2f15e4a7c46
LeoAndo <ando@LeoAndonoMacBook-Pro.local>
2021-04-07 18:27:04 +0900

    Java7までの剰余演算による余りの算出

--- Java8PlayGround/src/App.java
+++ Java8PlayGround/src/App.java
@@ -1,6 +1,9 @@
+import java.util.stream.IntStream;
+
 public class App {
     public static void main(String[] args) throws Exception {
         delimiter();
+        floorMod();
     }
 
     private static void delimiter() {
@@ -9,5 +12,12 @@ public class App {
         String c = "C";
         String value = String.join(", ", a, b, c);
         System.out.println("delimiter: " + value);
+    }
+
+    private static void floorMod() {
+        int[] results = IntStream.range(-5, 5).filter(x -> (x % 2 == 0)).toArray();
+        for (int result : results) {
+            System.out.println("floorMod: " + result);
+        }
     }
 }
