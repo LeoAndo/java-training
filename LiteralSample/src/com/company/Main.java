febc994b939870798d659f2fa7cbdbde5c50cb21
Leo Ando <ando.reo420@gmail.com>
2021-04-08 16:06:39 +0900

    浮動小数点数リテラルの実装サンプル

--- LiteralSample/src/com/company/Main.java
+++ LiteralSample/src/com/company/Main.java
@@ -3,28 +3,43 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         literalInt();
+        literalDouble();
     }
+
+    private static void literalDouble() {
+        System.out.println("========literalInt-START========");
+        // 10進数表記
+        double a = 12.34;
+        System.out.println("a: " + a);
+        // 指数表記 (eまたはEで指数を表す)
+        double b = 2.34e3; // 2.34 × 10の3乗(1000) = 2340.0
+        double c = 2.34e-3; // 2.34 × 10の-3乗(0.001) = 0.00234
+        System.out.println("b: " + b);
+        System.out.println("c: " + c);
+        System.out.println("========literalInt-END========");
+    }
+
     private static void literalInt() {
         System.out.println("========literalInt-START========");
         // 16進数を扱った整数リテラル (先頭に0xとつける)
         int hexA = 0xA; // 10
         int hexB = 0xB; // 11
-        System.out.println("16: a + b = " + (hexA+hexB));
+        System.out.println("16: a + b = " + (hexA + hexB));
 
         // 10進数を扱った整数リテラル
         int a = 10;
         int b = 11;
-        System.out.println("10: a + b = " + (a+b));
+        System.out.println("10: a + b = " + (a + b));
 
         // 8進数を扱った整数リテラル  (先頭に0とつける)
         int baseA = 012; // 10
         int baseB = 013; // 11
-        System.out.println("8: a + b = " + (baseA+baseB));
+        System.out.println("8: a + b = " + (baseA + baseB));
 
         // 2進数を扱った整数リテラル  (java7以降: 先頭に"0b" または "0B"とつける)
         int bA = 0b1010; // 10
         int bB = 0b1011; // 11
-        System.out.println("2: a + b = " + (bA+bB));
+        System.out.println("2: a + b = " + (bA + bB));
 
         System.out.println("========literalInt-END========");
     }
