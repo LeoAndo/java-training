7405850042d7b747aa353b12d1521a9879027880
Leo Ando <ando.reo420@gmail.com>
2021-04-08 20:15:25 +0900

    テンプレートプロジェクトをコミット

--- OperatorSample/src/com/company/Main.java
+++ OperatorSample/src/com/company/Main.java
@@ -2,8 +2,13 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
+        // 加算代入
         int x = 100;
         x = x + 10;
+        System.out.println(x);
+
+        // インクリメント
+        x = x + 1;
         System.out.println(x);
     }
 }
