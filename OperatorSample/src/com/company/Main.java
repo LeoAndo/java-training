bfee94423c90d65eca99fd3696da439a68ca5513
Leo Ando <ando.reo420@gmail.com>
2021-04-19 18:17:57 +0900

    単項演算子を使うサンプル

--- OperatorSample/src/com/company/Main.java
+++ OperatorSample/src/com/company/Main.java
@@ -14,5 +14,24 @@ public class Main {
         // 割り算
         System.out.println(10 / 3);
         System.out.println(10 / 3.0);
+
+        // 単項演算子を使うサンプル.
+        testUnaryOperator();
+    }
+
+    private static void testUnaryOperator() {
+        System.out.println("===========testUnaryOperator-START=============");
+        int a = 10;
+        int b;
+        b = ++a;
+        System.out.println(a);
+        System.out.println(b);
+
+        int c = 10;
+        int d;
+        d = c++;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println("===========testUnaryOperator-END=============");
     }
 }
