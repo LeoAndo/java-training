baf4ac25b1e05ec6039827dc2101fc9f3cdea459
Leo Ando <ando.reo420@gmail.com>
2021-04-19 17:46:39 +0900

    整数同士の割り算と浮動小数点数ありの割り算

--- OperatorSample/src/com/company/Main.java
+++ OperatorSample/src/com/company/Main.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Main {
     public static void main(String[] args) {
         // 加算代入
@@ -10,5 +13,9 @@ public class Main {
         // インクリメント
         x++;
         System.out.println(x);
+
+        // 割り算
+        System.out.println(10 / 3);
+        System.out.println(10 / 3.0);
     }
 }
