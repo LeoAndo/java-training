e0ec77b29f34e85c73015ba8fc075cfd59d06da1
Leo Ando <ando.reo420@gmail.com>
2021-04-22 22:13:11 +0900

    丸め誤差の対処方法のサンプル

---
+++ LoopSample/src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        testWhileLoop();
+        testDoWhileLoop();
+    }
+
+    private static void testWhileLoop() {
+        System.out.println("============testWhileLoop-START===========");
+        int num = 0;
+
+        while(num < 5) {
+            System.out.println(num);
+            num++;
+        }
+        System.out.println("============testWhileLoop-END===========");
+    }
+
+    private static void testDoWhileLoop() {
+
+    }
+}
