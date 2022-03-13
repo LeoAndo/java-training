e8998672c8985d51d203d25ff9c1b0267f1cead0
LeoAndo <leo.777.info@gmail.com>
2022-03-13 14:32:54 +0900

    インスタンスの個数を制限するsingltonパターンのサンプル(recordにリファクタリング)

--- Singleton/src/com/example/ex02/Triple.java
+++ Singleton/src/com/example/ex02/Triple.java
@@ -4,18 +4,16 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Triple {
-    private static final Map<String,Triple> map = new HashMap<>();
+public record Triple(String name) {
+    private static final Map<String, Triple> map = new HashMap<>();
+
     static {
-        String[] names = { "ALPHA", "BETA", "GAMMA" };
+        String[] names = {"ALPHA", "BETA", "GAMMA"};
         Arrays.stream(names).forEach(s -> map.put(s, new Triple(s)));
     }
 
-    private final String name;
-
-    private Triple(String name) {
+    public Triple {
         System.out.println("The instance " + name + " is created.");
-        this.name = name;
     }
 
     public static Triple getInstance(String name) {
