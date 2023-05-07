59becc986d0cb37bc93efdd39f7b2f7847e9aa80
ando <leo81@outlook.jp>
2023-05-07 16:19:52 +0900

    Decoratorパターンのサンプル

--- DecoratorDesignPattern/src/Main.java
+++ DecoratorDesignPattern/src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final Display b1 = new StringDisplay("Hello, world!");
+        final Display b2 = new SideBorder(b1, '#');
+        final Display b3 = new FullBorder(b2);
+        b1.show();
+        b2.show();
+        b3.show();
+        final Display b4 = new SideBorder(
+                new FullBorder(
+                        new FullBorder(
+                                new SideBorder(
+                                        new FullBorder(
+                                                new StringDisplay("Hello, word.")
+                                        ),
+                                        '*'
+                                )
+                        )
+                ),
+                '/'
+        );
+        b4.show();
     }
 }
---
+++ DecoratorDesignPattern/src/StringDisplay.java
@@ -0,0 +1,28 @@
+/**
+ * 1行の文字列を表示するクラス
+ */
+public class StringDisplay extends Display {
+    private final String string; // 表示文字列
+
+    public StringDisplay(String string) {
+        this.string = string;
+    }
+
+    @Override
+    public int getColumns() {
+        return string.length();
+    }
+
+    @Override
+    public int getRows() {
+        return 1; // 行数は1
+    }
+
+    @Override
+    public String getRowText(int row) {
+        if (row != 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        return string;
+    }
+}
